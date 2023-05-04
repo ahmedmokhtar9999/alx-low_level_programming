6c1947f1c46cadddc823ea774242daac87c4d16a
ahmedmokhtar9999 <ah.mokh9999@gmail.com>
2023-05-03 19:10:12 -0700

    0x13. C - More singly linked lists0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,7 @@
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
- * Return: pointer to the new node, or NULL
+ * return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -17,7 +17,6 @@ listint_t *temp = *head;
 new = malloc(sizeof(listint_t));
 if (!new || !head)
 return (NULL);
-
 new->n = n;
 new->next = NULL;
 
